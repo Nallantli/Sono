f4c7e3193c7db4b78da1f49b51ed316e38d2ca4f
Agilulfulus <parkbenjamin2@gmail.com>
2020-08-27 00:30:58 -0400

    more bug fix

--- bin/sono.sh
+++ bin/sono.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 SCRIPT=$(readlink -f "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
-java "-Dfile.encoding=UTF-8" -jar "$SCRIPTPATH/SonoClient.jar:$SCRIPTPATH/external/*" client.SonoClient $*
+java "-Dfile.encoding=UTF-8" -cp "$SCRIPTPATH/SonoClient.jar:$SCRIPTPATH/external/*" client.SonoClient $*
